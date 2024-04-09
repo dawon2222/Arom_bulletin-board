c5ac789b54e2e61dfa2dd3e83fcdae748dd462ee
dawon222 <da5720@naver.com>
2024-04-09 16:49:09 +0900

    DB URI 삭제

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=new express();
 app.use(express.json());
 app.use('/post',PostRouter);//응답받는 url을 고치고 싶으면 문자열 추가
 
-mongoose.connect("mongodb+srv://tkrhk5720:1234@cluster0.igj3zvn.mongodb.net/Arom?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("");
 
 app.listen(8000,() =>{//이 메쏘드는 서버를 대기상태로 만듦 외부에서 들어오는 요청을 이 서버가 언제든지 처리 가능하도록 함
     console.log("서버 실행됨");
